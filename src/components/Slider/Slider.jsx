3fd662f63385ea69ec2a4884483a3af36c1f77c2
Konrad Nojman <94konrad.n@gmail.com>
2019-06-26 00:27:39 +0200

    simplified css of components on mobile version

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -8,8 +8,8 @@ const Slider = () => {
 			<h2>TOP DESIGNERS</h2>
 			<button>CHECK US!</button>
 			<div className="show-more-wrapper">
-				<span class="lnr lnr-chevron-down" />
-				<span class="lnr lnr-chevron-down" />
+				<span className="lnr lnr-chevron-down" />
+				<span className="lnr lnr-chevron-down" />
 			</div>
 			<div className="slider-bg" />
 		</div>
