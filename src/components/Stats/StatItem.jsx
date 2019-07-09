db385bba753752ded8c6e29f9c5ba5ff27ef8c2c
Konrad Nojman <94konrad.n@gmail.com>
2019-07-09 23:44:48 +0200

    animated stats circles and numbers

--- src/components/Stats/StatItem.jsx
+++ src/components/Stats/StatItem.jsx
@@ -1,32 +1,57 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { Component } from "react";
 import CircularProgress from "@bit/mui-org.material-ui.circular-progress";
+import CountUp from "react-countup";
 
-const StateItem = props => {
-	const { number, title, description, className } = props;
-	return (
-		<div className="stat-wrapper">
-			<div className={`circle ${className}`}>
-				<CircularProgress
-					variant="static"
-					value={number}
-					thickness={1}
-					size={150}
-				/>
-				<CircularProgress
-					variant="static"
-					value={100}
-					thickness={1}
-					size={150}
-				/>
+class StateItem extends Component {
+	state = {
+		value: 0
+	};
+	componentDidMount = () => {
+		setTimeout(() => {
+			this.setState({ value: this.props.number });
+		}, 0);
+	};
 
-				<h2>{number} </h2>
-				<h3>{title}</h3>
+	render() {
+		const { number, title, description, className } = this.props;
+		return (
+			<div className="stat-wrapper">
+				<div className={`circle ${className}`}>
+					<CircularProgress
+						className="stat__circle__progress"
+						variant="static"
+						value={this.state.value}
+						thickness={1}
+						size={150}
+					/>
+					<CircularProgress
+						className="stat__circle__bg"
+						variant="static"
+						value={100}
+						thickness={1}
+						size={150}
+					/>
+
+					<h2>
+						<CountUp
+							end={number}
+							duration={3}
+							easingFn={function(t, b, c, d) {
+								var ts = (t /= d) * t;
+								var tc = ts * t;
+								return b + c * (tc + -3 * ts + 3 * t);
+							}}
+							// onEnd={() => this.setState({ value: number })}
+						/>
+					</h2>
+					<h3>{title}</h3>
+				</div>
+				<p className="description">{description}</p>
 			</div>
-			<p className="description">{description}</p>
-		</div>
-	);
-};
+		);
+	}
+}
 
 StateItem.propTypes = {
 	description: PropTypes.string.isRequired,
