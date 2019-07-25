d0372b179e5cc5e7e7a237e4946cd1a1674e3c7f
Konrad Nojman <94konrad.n@gmail.com>
2019-07-26 00:14:50 +0200

    refactored social icons to component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import "./Header.css";
 import Nav from "../Nav/Nav";
+
+const HeaderIcon = ({iconClass}) => {
+	return (
+		<React.Fragment>
+			{/* eslint-disable-next-line */}
+			<a href="#" className="header__social-icons__link">
+				<i className={`fab ${iconClass}`} />
+			</a>
+		</React.Fragment>
+	);
+}
+
+const iconClasses = ["fa-facebook-f", "fa-instagram", "fa-twitter", "fa-linkedin"];
+
 const Header = () => {
 	return (
 		<header className="header content-wrapper">
@@ -10,22 +24,7 @@ const Header = () => {
 					<a href="#">DSGN</a>
 				</h2>
 				<div className="header__social-icons">
-					{/* eslint-disable-next-line */}
-					<a href="#" className="header__social-icons__link">
-						<i className="fab fa-facebook-f" />
-					</a>
-					{/* eslint-disable-next-line */}
-					<a href="#" className="header__social-icons__link">
-						<i className="fab fa-instagram" />
-					</a>
-					{/* eslint-disable-next-line */}
-					<a href="#" className="header__social-icons__link">
-						<i className="fab fa-twitter" />
-					</a>
-					{/* eslint-disable-next-line */}
-					<a href="#" className="header__social-icons__link">
-						<i className="fab fa-linkedin" />
-					</a>
+					{iconClasses.map((item, index) => <HeaderIcon key={index} iconClass={item}/>)}
 				</div>
 			</div>
 			<Nav />
