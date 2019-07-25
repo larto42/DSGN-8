9893b6cfe324383544a0cbee62b453352a9d1f6d
Konrad Nojman <94konrad.n@gmail.com>
2019-07-26 00:48:07 +0200

    refactored stats to generate from array of data

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -3,26 +3,38 @@ import StatItem from "./StatItem";
 import "./Stats.css";
 
 const Stats = () => {
+	const stats = [
+		{
+			className: "projects-stat",
+			title: "Big projects",
+			number: 50,
+			description: "Our experience shows in projects we've made!",
+		},
+		{
+			className: "client-stat",
+			title: "Clients",
+			number: 35,
+			description: "Customer satisfaction is the most important for us. That's why we care about every detail.",
+		},
+		{
+			className: "month-stat",
+			title: "Months",
+			number: 80,
+			description: "Over 6 years of constant innovation makes us the best partner for your new project!",
+		},
+	];
+	
 	return (
 		<div className="stats content-wrapper">
-			<StatItem
-				className="projects-stat"
-				title="Big projects"
-				number={50}
-				description="Our experience shows in projects we've made!"
-			/>
-			<StatItem
-				className="client-stat"
-				title="Clients"
-				number={35}
-				description="Customer satisfaction is the most important for us. That's why we care about every detail."
-			/>
-			<StatItem
-				className="month-stat"
-				title="Months"
-				number={80}
-				description="Over 6 years of constant innovation makes us the best partner for your new project!"
-			/>
+			{stats.map((item, index) => 
+				<StatItem
+					key={index}
+					className={item.className}
+					title={item.title}
+					number={item.number}
+					description={item.description}
+				/>
+			)}
 		</div>
 	);
 };
