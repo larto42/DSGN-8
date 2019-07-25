8280ab6d892b6faa2a2bbb73df148f20eca05fb9
Konrad Nojman <94konrad.n@gmail.com>
2019-07-26 01:01:27 +0200

    refactored menu items to generate from data array

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,6 +8,21 @@ const Nav = () => {
 		setOpen(!isOpen);
 	};
 
+	const menuItems = [
+		{
+			href: "#team",
+			text: "Our team"
+		},
+		{
+			href: "#portfolio",
+			text: "Portfolio"
+		},
+		{
+			href: "#brands",
+			text: "Brands"
+		},
+	];
+
 	return (
 		<nav className="menu__wrapper" onClick={toogleMenu}>
 			<button
@@ -19,31 +34,15 @@ const Nav = () => {
 				<span className="menu__text">Menu</span>
 			</button>
 			<ul className={"menu__list" + (isOpen ? " opened" : "")}>
-				<li className="menu__item">
-					<a className="menu__item__link" href="#team" onClick={toogleMenu}>
-						<div className="menu__item__text-wrapper">
-							<span className="menu__item__text">Our team</span>
-						</div>
-					</a>
-				</li>
-				<li className="menu__item">
-					<a
-						href="#portfolio"
-						className="menu__item__link"
-						onClick={toogleMenu}
-					>
-						<div className="menu__item__text-wrapper">
-							<span className="menu__item__text">Portfolio</span>
-						</div>
-					</a>
-				</li>
-				<li className="menu__item">
-					<a href="#brands" className="menu__item__link" onClick={toogleMenu}>
-						<div className="menu__item__text-wrapper">
-							<span className="menu__item__text">Brands</span>
-						</div>
-					</a>
-				</li>
+				{menuItems.map((item, index) => 
+					<li key={index} className="menu__item">
+						<a className="menu__item__link" href={item.href} onClick={toogleMenu}>
+							<div className="menu__item__text-wrapper">
+								<span className="menu__item__text">{item.text}</span>
+							</div>
+						</a>
+					</li>
+				)}
 			</ul>
 		</nav>
 	);
